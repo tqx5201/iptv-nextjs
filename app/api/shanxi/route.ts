4722788833c895e73f0a0deef3cc05579d59730b
天仙婆婆 <tqx5201@163.com>
2026-06-24 06:49:24 +0800

    Update route.ts

--- app/api/shanxi/route.ts
+++ app/api/shanxi/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getRealHost } from '../utils/url';
 
-export const runtime = 'edge';
+//export const runtime = 'edge';
 
 // 频道配置
 const CHANNELS: Record<string, string> = {
